03a9eedadaa38f7a3712332a774b5ff362ef7993
Kennet Santiago Sánchez Roldán <78995084+KennetSanchez@users.noreply.github.com>
2024-05-15 14:22:01 +0200

    Books search methods working
    The search and list methods are working, some of the CRUD basics seems to be working too.
    
    All of them would appreciate a further styling

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -6,14 +6,22 @@ use App\Entity\Book;
 use App\Form\BookType;
 use App\Repository\BookRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use ErrorException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/book')]
+#[Route('/books')]
 class BookController extends AbstractController
 {
+    private function decodeRequest(Request $request){
+        $content = $request->getContent();
+        return json_decode($content);
+    }
+
     #[Route('/', name: 'app_book_index', methods: ['GET'])]
     public function index(BookRepository $bookRepository): Response
     {
@@ -42,15 +50,7 @@ class BookController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_book_show', methods: ['GET'])]
-    public function show(Book $book): Response
-    {
-        return $this->render('book/show.html.twig', [
-            'book' => $book,
-        ]);
-    }
-
-    #[Route('/{id}/edit', name: 'app_book_edit', methods: ['GET', 'POST'])]
+    #[Route('/{id}/edit', name: 'app_book_edit', methods: ['PUT'])]
     public function edit(Request $request, Book $book, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(BookType::class, $book);
@@ -68,7 +68,7 @@ class BookController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_book_delete', methods: ['POST'])]
+    #[Route('/{id}', name: 'app_book_delete', methods: ['DELETE'])]
     public function delete(Request $request, Book $book, EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete'.$book->getId(), $request->getPayload()->get('_token'))) {
@@ -78,4 +78,129 @@ class BookController extends AbstractController
 
         return $this->redirectToRoute('app_book_index', [], Response::HTTP_SEE_OTHER);
     }
+
+    #[Route('/library', name: 'app_book_find_with_library', methods: ['GET'])]
+    public function findByLibrary(BookRepository $bookRepository, Request $request) : Response {
+        $decoded = $this->decodeRequest($request);
+
+        try{
+            $libraryName = $decoded->libraryName;
+            $books = $bookRepository->findByLibrary($libraryName);
+            return $this->render('book/show.html.twig',[
+                'books' => $books,
+                'searchType' => sprintf('Libros de la biblioteca %s', $libraryName)
+            ]);
+        }catch (ErrorException $exception){
+            throw new HttpException(statusCode: Response::HTTP_BAD_REQUEST, message: "Parece haber un problema con los parámetros de la petición. ".$exception->getMessage());
+        }
+    }
+
+
+    #[Route('/title', name: 'app_book_find_with_title', methods: ['GET'], format: 'json')]
+    public function findByTitle(BookRepository $bookRepository, Request $request): Response
+    {
+        
+        $decoded = $this->decodeRequest($request);
+
+        try{
+            $title = $decoded->title;
+            $criteria = ['title'=>$title];
+            $books = $bookRepository->findBy($criteria);
+
+            return $this->render('book/show.html.twig', [
+                'books' => $books,
+                'searchType' => sprintf('Libros con título %s', $title),
+            ]);
+
+        }catch(ErrorException $exception){
+            throw new HttpException(statusCode: Response::HTTP_BAD_REQUEST, message: "Parece haber un error con los parámetros de la petición. ".$exception->getMessage());
+        }
+    }
+
+    #[Route('/titleAndAvailability', name: 'app_book_find_with_name_and_copies', methods: ['GET'])]
+    public function findByTitleAndAvailability(BookRepository $bookRepository, Request $request): Response
+    {
+        $decoded = $this->decodeRequest($request);
+
+
+        try{
+            $title = $decoded->title;
+            $query = $bookRepository->findByTitleAndAvailabilityJoinedToLibrary($title);
+
+            return $this->render('book/show_with_library.html.twig', [
+                'queryResult' => $query,
+                'searchTitle' => sprintf('Libros disponibles con título %s', $title),
+            ]);
+
+        }catch(ErrorException $exception){
+            throw new HttpException(statusCode: Response::HTTP_BAD_REQUEST, message: "Parece haber un error con los parámetros de la petición. ".$exception->getMessage());
+        }
+    }
+
+    #[Route('/titleAndLibrary', name: 'app_book_find_with_title_and_library', methods: ['GET'])]
+    public function findByTitleAndLibrary(BookRepository $bookRepository, Request $request) : Response {
+        $decoded = $this->decodeRequest($request);
+
+        try {
+            $title = $decoded->title;
+            $libraryName = $decoded->libraryName;
+            $results = $bookRepository->findByTitleAndLibraryJoinedToLibrary(title: $title, libraryName: $libraryName);
+        }catch (ErrorException $exception){
+            throw new HttpException(
+                statusCode: Response::HTTP_BAD_REQUEST,
+                message: "Parece haber un problema con los parámetros de la petición. ".$exception->getMessage()
+            );
+        }
+
+        return $this->render('book/show_with_library.html.twig', [
+            'queryResult' => $results,
+            'searchTitle' => sprintf('Libros con título %s y biblioteca %s', $title, $libraryName),
+        ]);
+    }
+
+    #[Route('/publisherAndLibrary', name: 'app_book_find_with_publisher_and_library', methods: ['GET'])]
+    public function findByPublisherAndLibrary(BookRepository $bookRepository, Request $request) : Response {
+        $decoded = $this->decodeRequest($request);
+
+        try {
+            $publisher = $decoded->publisher;
+            $libraryName = $decoded->libraryName;
+            $results = $bookRepository->findByPublisherAndLibraryJoinedToLibrary(publisher: $publisher, libraryName: $libraryName);
+        }catch (ErrorException $exception){
+            throw new HttpException(
+                statusCode: Response::HTTP_BAD_REQUEST,
+                message: "Parece haber un problema con los parámetros de la petición. ".$exception->getMessage()
+            );
+        }
+
+        return $this->render('book/show_with_library.html.twig', [
+            'queryResult' => $results,
+            'searchTitle' => sprintf('Libros con editorial %s y biblioteca %s', $publisher, $libraryName),
+        ]);
+    }
+
+    #[Route('/authorAndLibrary', name: 'app_book_find_with_author_and_library', methods: ['GET'])]
+    public function findByAuthorAndLibrary(BookRepository $bookRepository, Request $request) : Response {
+        $decoded = $this->decodeRequest($request);
+
+        try {
+            $author = $decoded->author;
+            $libraryName = $decoded->libraryName;
+            $results = $bookRepository->findByAuthorAndLibraryJoinedToLibrary(author: $author, libraryName: $libraryName);
+        }catch (ErrorException $exception){
+            throw new HttpException(
+                statusCode: Response::HTTP_BAD_REQUEST,
+                message: "Parece haber un problema con los parámetros de la petición. ".$exception->getMessage()
+            );
+        }
+
+        return $this->render('book/show_with_library.html.twig', [
+            'queryResult' => $results,
+            'searchTitle' => sprintf('Libros del autor %s y biblioteca %s', $author, $libraryName),
+        ]);
+    }
+
+
+
+
 }
--- src/Repository/BookRepository.php
+++ src/Repository/BookRepository.php
@@ -16,28 +16,89 @@ class BookRepository extends ServiceEntityRepository
         parent::__construct($registry, Book::class);
     }
 
-//    /**
-//     * @return Book[] Returns an array of Book objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('b')
-//            ->andWhere('b.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('b.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+//        TODO: Update the querys so they only takes the params that we are going to use
 
-//    public function findOneBySomeField($value): ?Book
-//    {
-//        return $this->createQueryBuilder('b')
-//            ->andWhere('b.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+
+    public function findByTitleAndAvailabilityJoinedToLibrary(string $title): array
+    {
+
+        $entityManager = $this->getEntityManager();
+
+        $query = $entityManager->createQueryBuilder()
+            ->select('Books', 'Library')
+            ->from('App\Entity\Book', 'Books')
+            ->innerJoin('Books.library', 'Library')
+            ->where('Books.title = :title')
+            ->andWhere('Books.copies >  0')
+            ->setParameter('title', $title)
+            ->getQuery();
+
+        return $query->getResult();
+    }
+
+    public function findByPublisherAndLibraryJoinedToLibrary(string $publisher, string $libraryName): array
+    {
+        $entityManager = $this->getEntityManager();
+
+        $query = $entityManager->createQueryBuilder()
+            ->select('Books', 'Library')
+            ->from('App\Entity\Book', 'Books')
+            ->innerJoin('Books.library', 'Library')
+            ->where('Books.publisher = :publisher')
+            ->andWhere('Library.name = :libraryName')
+            ->setParameter('publisher', $publisher)
+            ->setParameter('libraryName', $libraryName)
+            ->getQuery();
+
+        return $query->getResult();
+    }
+
+    public function findByAuthorAndLibraryJoinedToLibrary(string $author, string $libraryName): array
+    {
+        $entityManager = $this->getEntityManager();
+
+        $query = $entityManager->createQueryBuilder()
+            ->select('Books', 'Library')
+            ->from('App\Entity\Book', 'Books')
+            ->innerJoin('Books.library', 'Library')
+            ->where('Books.author = :author')
+            ->andWhere('Library.name = :libraryName')
+            ->setParameter('author', $author)
+            ->setParameter('libraryName', $libraryName)
+            ->getQuery();
+
+        return $query->getResult();
+    }
+
+    public function findByTitleAndLibraryJoinedToLibrary(string $title, string $libraryName): array
+    {
+        $entityManager = $this->getEntityManager();
+
+        $query = $entityManager->createQueryBuilder()
+            ->select('Books', 'Library')
+            ->from('App\Entity\Book', 'Books')
+            ->innerJoin('Books.library', 'Library')
+            ->where('Books.title = :title')
+            ->andWhere('Library.name = :libraryName')
+            ->setParameter('title', $title)
+            ->setParameter('libraryName', $libraryName)
+            ->getQuery();
+
+        return $query->getResult();
+    }
+
+    public function findByLibrary(string $libraryName) : array
+    {
+        $entityManager = $this->getEntityManager();
+
+        $query = $entityManager->createQueryBuilder()
+            ->select('Books')
+            ->from('App\Entity\Book', 'Books')
+            ->innerJoin('Books.library', 'Library')
+            ->where('Library.name = :libraryName')
+            ->setParameter('libraryName', $libraryName)
+            ->getQuery();
+
+        return $query->getResult();
+    }
 }
